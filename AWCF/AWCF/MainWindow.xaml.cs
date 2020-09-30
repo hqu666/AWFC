dbbb96913eb8fb6e66e443dc1c1cc5182152ee6c
hiroomi kuwayama <hqu666@gmai.com>
2020-10-01 00:36:06 +0900

    web

--- AWCF/AWCF/MainWindow.xaml.cs
+++ AWCF/AWCF/MainWindow.xaml.cs
@@ -14,13 +14,29 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AWCF {
+
 	/// <summary>
 	/// MainWindow.xaml の相互作用ロジック
 	/// </summary>
-	public partial class MainWindow : Window {
+	public partial class MainWindow : NavigationWindow {
+	
+	private static WebWindow webW = null;
 		public MainWindow()
 		{
 			InitializeComponent();
+			callWeb();
 		}
+
+		private void callWeb(){
+			if (webW == null) {
+				// 次に表示するページ（ページ2）を生成、以後使いまわし
+				webW = new WebWindow();
+			}
+
+			// ページ2へ移動
+			this.NavigationService.Navigate(webW);
+
+		}
+
 	}
 }
